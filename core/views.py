e11ae9db5f211ebb2d7fe1561089cd66dfa72162
Aredhel <aredhel@email.cz>
2010-01-25 07:32:04 +0100

    displaying recipe name and ingredients

--- core/views.py
+++ core/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from django.shortcuts import get_object_or_404, render_to_response
+from django.utils.encoding import force_unicode
+from models import Recipe
+import re
+
+def recipe_detail(request, recipe_id):
+    r = get_object_or_404(Recipe, pk=recipe_id)
+    #normalize line breaks - see django.utils.html.linebreaks
+    ingredients = re.sub(r'\r\n|\r|\n','\n', force_unicode(r.ingredients))
+    ingredient_list = re.split('\n{1,}',ingredients)
+    
+    """ 
+    print ingredients
+    print ingredient_list
+
+    ingredient_list = ['a','b','c']
+    """
+            
+    return render_to_response('core/recipe.html', 
+            {'recipe': r,
+             'ingredient_list': ingredient_list})
--- urls.py
+++ urls.py
@@ -4,13 +4,14 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('edesia',
     # Example:
     # (r'^edesia/', include('edesia.foo.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^recipes/(?P<recipe_id>\d+)/$','core.views.recipe_detail'),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
