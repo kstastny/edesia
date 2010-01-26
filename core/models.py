65253fdf226297ac8238b70da48468976d50a39d
Aredhel <aredhel@email.cz>
2010-01-26 08:04:14 +0100

    renamed model field, added displaying of directions

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
     ingredients = models.TextField()
-    description = models.TextField()
+    directions = models.TextField()
     primary_photo = models.CharField(max_length=255, blank=True)
     inserted = models.DateField(auto_now_add=True)
     tags = models.ManyToManyField('Tag', blank=True)
