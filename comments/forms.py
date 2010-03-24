8105624fe4da728cc2bd4835f964d75567743483
Aredhel <aredhel@email.cz>
2010-03-24 17:43:47 +0100

    Added asterisk to required comment fields

--- comments/forms.py
+++ comments/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from django.contrib.comments.forms import CommentForm as DjangoCommentForm
 
 class CommentForm(DjangoCommentForm):
-    email         = forms.EmailField(label="Email")
+    email         = forms.EmailField(label="Email", required=False)
 
