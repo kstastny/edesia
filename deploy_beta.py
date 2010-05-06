dd63654d7557468fd9190f1280dc73e6b88d8794
aredhel@email.cz <>
2010-05-06 15:09:03 +0200

    Finished deployment script for generating deploy package

--- deploy_beta.py
+++ deploy_beta.py
@@ -1,14 +1,23 @@
 #not sure how to run admin commands from python, running them using command line
 import sys
+import os
 import paramiko
+import shutil
 
 from subprocess import call, Popen, PIPE
 from datetime import datetime
 from getpass import getpass
+from os import path
+from os.path import join
+
+#http://stackoverflow.com/questions/2584414/python-windows-file-copy-with-wildcard-supporthtaccess
+def copy_files(src_glob, dst_folder):
+    for fname in iglob(src_glob):
+        copy(fname, join(dst_folder, fname))
+
 
 settingsArgument = '--settings=settings.web4ce'
 
-""" TODO uncomment
 print 'Running syncdb...'
 call(['python', 'manage.py', 'syncdb', settingsArgument])
 
@@ -24,9 +33,54 @@ backup_data = Popen(['python', 'manage.py', 'dumpdata', settingsArgument, '--exc
 backup_file = open(filename, 'w')
 backup_file.write(backup_data)
 backup_file.close()
+
+print 'Preparing distribution package...'
+DIST_DIRECTORY = '_dist'
+#remove the directory if it exists
+if os.access(DIST_DIRECTORY, os.F_OK):
+    shutil.rmtree(DIST_DIRECTORY)
+
+
+os.mkdir(DIST_DIRECTORY)
+os.mkdir(join(DIST_DIRECTORY, 'logs')) #the log directory is not automatically created
+
+shutil.copy(join('deployment', '.htaccess'), DIST_DIRECTORY)
+shutil.copy(join('deployment', 'django.wsgi.production'), join(DIST_DIRECTORY, 'django.wsgi'))
+shutil.copy(join('deployment', 'google0d1d58e1af972248.html'), DIST_DIRECTORY) #google validation
+shutil.copy(join('deployment', 'robots.txt'), DIST_DIRECTORY)
+
+deploy_dir = join(DIST_DIRECTORY, 'edesia')
+os.mkdir(deploy_dir)
+
+print 'Copying application files...'
+shutil.copy(join('deployment', '__init__.py'), deploy_dir)
+shutil.copy('urls.py', deploy_dir)
+shutil.copy('manage.py', deploy_dir)
+
+shutil.copytree('comments', join(deploy_dir, 'comments'))
+shutil.copytree('contact', join(deploy_dir, 'contact'))
+shutil.copytree('core', join(deploy_dir, 'core'))
+shutil.copytree('djangoratings', join(deploy_dir, 'djangoratings'))
+shutil.copytree('south', join(deploy_dir, 'south'))
+shutil.copytree('users', join(deploy_dir, 'users'))
+
+print 'Copying media files and templates...'
+shutil.copytree('templates', join(deploy_dir, 'templates'))
+shutil.copytree('site_media', join(deploy_dir, 'site_media'))
+
+#copy settings correctly - web4ce
+print 'Copying settings files...'
+settings_dir = join(deploy_dir, 'settings')
+os.mkdir(settings_dir)
+shutil.copy(join('settings', '__init__.py'), settings_dir)
+shutil.copy(join('settings', 'common.py'), settings_dir)
+shutil.copy(join(join('settings', 'web4ce'), 'custom.py'), settings_dir)
+
+
+
+
+
 """
-
-
 #TODO copy data to server - needs paramiko
 print 'Copying deployment files to beta deployment...'
 print 'Please enter FTP password:'
@@ -44,3 +98,4 @@ transport.connect(username = username, password = password)
 #TODO finish
 sftp = paramiko.SFTPClient.from_transport(transport)
 sftp.rmdir(ftp_directory)
+"""
---
+++ deployment/urls.py
@@ -0,0 +1,48 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.core.urlresolvers import reverse
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    #(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page':'/'}, name='logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'auth/login.html'}, name='login'),
+    url(r'^register/$', 'edesia.users.views.register', name='register'),
+    url(r'^comments/', include('django.contrib.comments.urls')),
+    )
+
+#TODO move recipe patterns to core, authentication patterns to auth
+urlpatterns += patterns('edesia',
+    # Example:
+    # (r'^edesia/', include('edesia.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^recipes/$','core.views.recipe_list', {'tag_slug':None}, name='recipe_list'),
+    url(r'^recipes/(?P<tag_slug>[\w-]+)/$','core.views.recipe_list', name='recipe_list'),
+    url(r'^recipe/(?P<recipe_slug>[\w-]+)/$','core.views.recipe_detail', name='recipe_detail'),
+    url(r'^recipe_edit/$','core.views.recipe_edit', {'recipe_slug':None}, name='recipe_edit'),
+    url(r'^recipe_edit/(?P<recipe_slug>[\w-]+)/$','core.views.recipe_edit',name='recipe_edit'),
+    url(r'^recipe_rate/(?P<recipe_id>[0-9]+)/$','core.views.rate_recipe',name='recipe_rate'),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+
+    url(r'^query/$','core.views.searchquery', name='search_query'),
+
+
+    url(r'^categories/$', 'core.views.category_list', name='category_list'),
+    #TODO use different home page
+    #url(r'^$', 'core.views.recipe_list', {'tag_slug':None}, name='home'),
+    url(r'^$', 'core.views.index', name='home'),
+
+)
+
+
+urlpatterns += patterns('',
+        (r'^contact/', include('edesia.contact.urls')),
+        )
