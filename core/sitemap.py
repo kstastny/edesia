fda8418b07a91ff068a501c888286f973e460a1a
Aredhel <aredhel@email.cz>
2010-10-12 20:57:36 +0200

    dynamically assigning sitemap priority to recipes - newer pages get higher priority

--- core/sitemap.py
+++ core/sitemap.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import logging
+
 from django.contrib.sitemaps import Sitemap
 from core.models import Recipe, Tag
 
@@ -9,6 +12,20 @@ class RecipeSitemap(Sitemap):
 
     def location(self, recipe):
         return '/recipe/%s' % recipe.slug
+
+    #http://www.sitemaps.org/protocol.php - valid priority is 0.0 to 1.0
+    def priority(self, recipe):
+        """
+         return priority of recipe - the newer the recipe, the higher priority
+        """
+        recipe_age_days = datetime.now().toordinal() - recipe.inserted.toordinal() 
+        #use simple linear function, will suffice
+        priority = 1.0 - (recipe_age_days/14)*0.05
+        if priority < 0.25:
+            priority = 0.25
+        logging.debug('Recipe %s is %s days old, assigned sitemap priority is %s',\
+                recipe.name, recipe_age_days, priority)
+        return priority
 
 class TagSitemap(Sitemap):
     changefreq = 'weekly'
