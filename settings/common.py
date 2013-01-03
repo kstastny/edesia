f2e542be4f1d76201c2a521a7d6e5ab736f8f2d8
aredhel@email.cz <>
2013-01-03 17:29:51 +0100

    Fixed adding of comments

--- settings/common.py
+++ settings/common.py
@@ -53,6 +53,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'edesia.core.middleware.ErrorMiddleware',
