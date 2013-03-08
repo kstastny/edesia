0686a9d82b4f5a99adf4f632e77290acc765ccf2
aredhel@email.cz <>
2013-03-08 21:06:13 +0100

    Added simple spam protection

--- comments/forms.py
+++ comments/forms.py
@@ -1,6 +1,17 @@
+#coding=utf-8
+
 from django import forms
 from django.contrib.comments.forms import CommentForm as DjangoCommentForm
+from django.core.exceptions import ValidationError
+
+def validate_captcha(value):
+    if value <> '2':
+        raise ValidationError('Nesprávný výsledek.')
+
 
 class CommentForm(DjangoCommentForm):
     email         = forms.EmailField(label="Email", required=False)
-
+    captcha       = forms.CharField(label=u"Pro vložení komentáře spočtěte jednoduchý příklad: 1 + 1 = ?", 
+            required=True,
+            validators=[validate_captcha])
+            #error_messages={'invalid': u'Nesprávný výsledek.'})
