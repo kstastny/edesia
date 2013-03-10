60460a2c3857013d82ee9de41de623a30a0b06d4
aredhel@email.cz <>
2013-03-10 15:39:38 +0100

    Modified CAPTCHA question

--- comments/forms.py
+++ comments/forms.py
@@ -5,13 +5,13 @@ from django.contrib.comments.forms import CommentForm as DjangoCommentForm
 from django.core.exceptions import ValidationError
 
 def validate_captcha(value):
-    if value <> '2':
+    if value.strip().upper() <> 'HAVEL':
         raise ValidationError('Nesprávný výsledek.')
 
 
 class CommentForm(DjangoCommentForm):
     email         = forms.EmailField(label="Email", required=False)
-    captcha       = forms.CharField(label=u"Pro vložení komentáře spočtěte jednoduchý příklad: 1 + 1 = ?", 
+    captcha       = forms.CharField(label=u"Pro vložení komentáře napište příjmení prvního prezidenta ČR (Václav H.)", 
             required=True,
             validators=[validate_captcha])
             #error_messages={'invalid': u'Nesprávný výsledek.'})
